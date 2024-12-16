cc989746530120a8bef92c9b542e74765bc4b454
junyong23 <junyong5328@naver.com>
2024-12-16 23:38:48 +0900

    lms open 및 로그인 후 첫번째 class 진입
    셀레니움을 통해 사용자의 id 및 비밀번호를 입력칸에 입력하여 로그인 버튼까지 누르게 해주고,
    그 후 lms hanbat의 dashboard 창의 첫번째 과목의 class에 진입하게 한다

--- opensource_projuect_lms.py
+++ opensource_projuect_lms.py
@@ -1,3 +1,30 @@
-import webbrowser
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
-webbrowser.open_new('https://eclass.hanbat.ac.kr/xn-sso/login.php?auto_login=&sso_only=&cvs_lgn=&site=&return_url=https%3A%2F%2Feclass.hanbat.ac.kr%2Fxn-sso%2Fgw-cb.php%3Ffrom%3D%26site%3D%26login_type%3Dstandalone%26return_url%3D')
+driver = webdriver.Chrome()
+
+driver.get('https://lms.hanbat.ac.kr/')
+
+time.sleep(2)
+
+id_box = driver.find_element(By.ID, 'login_user_id')
+id_box.send_keys('20232071')
+
+pw_box = driver.find_element(By.ID, 'login_user_password')
+pw_box.click()
+time.sleep(1)
+pw_box.send_keys('cho182931!')
+
+time.sleep(1)
+login_btn = driver.find_element(By.CLASS_NAME, 'login_btn')
+login_btn.click()
+
+time.sleep(5)
+
+firstclass_btn = driver.find_element(By.CLASS_NAME, 'ic-DashboardCard')
+firstclass_btn.click()
+
+time.sleep(10)
+
