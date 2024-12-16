fbfe8a5bfd87ff06597bc10c46ee599e07306efd
junyong23 <junyong5328@naver.com>
2024-12-17 07:08:25 +0900

    코드 및 구현 기능 추가
    f = open 으로 바탕화면에 파일을 만들고, lms의 달력창에서 과제 등의 일정을 긁어서 fwrite 하는 코드 추가

--- opensource_project_lms.py
+++ opensource_project_lms.py
@@ -1,5 +1,8 @@
 from telnetlib import EC
 from datetime import datetime
+
+import uc
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -11,6 +14,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 options = Options()
 options.add_argument("--start-maximized")
+#options.add_argument("user-data-dir=C:\\Users\\junyo\\OneDrive\\바탕 화면\\Test")
+#options.add_argument("disable-blink-features=AutomationControlled")
 options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=options)
@@ -19,12 +24,13 @@ driver.get('https://lms.hanbat.ac.kr/')
 
 time.sleep(2)
 
-id_box = driver.find_element(By.ID, 'login_user_id')
-id_box.send_keys('20232071')
+id_btn = driver.find_element(By.ID, 'login_user_id')
+id_btn.send_keys('20232071')
 
-pw_box = driver.find_element(By.ID, 'login_user_password')
-pw_box.click()
-pw_box.send_keys('cho182931!')
+pw_btn = driver.find_element(By.ID, 'login_user_password')
+pw_btn.click()
+pw_btn.send_keys('cho182931!')
+
 login_btn = driver.find_element(By.CLASS_NAME, 'login_btn')
 login_btn.click()
 
@@ -37,14 +43,20 @@ time.sleep(1)
 now = datetime.now()
 month = now.month
 file_name = f"C:/Users/junyo/OneDrive/바탕 화면/{month}월 일정.txt"
-file_header = f"{month}월의 일정 입니다.\n"
+file_header = f"{month}월의 과제 및 일정 입니다.\n\n"
 
 f = open(file_name, "w")
 f.write(file_header)
-if(month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
-    for i in range(1, 32, 1):
-        f.write("Hello")
+
+test = driver.find_elements(By.CLASS_NAME, "fc-event-container")
+
+j = 1
+for i in test:
+    f.write(str(j) + ". -------------------------\n")
+    title = i.text
+    print(title+"\n")
+    f.write(title)
+    f.write("\n----------------------------\n\n\n")
+    j = j + 1
+
 f.close()
-
-
-time.sleep(15)
