c2eb72b3a08a0ae7fa4f5a0fa2f1963c49a93433
junyong23 <junyong5328@naver.com>
2024-12-17 01:25:17 +0900

    Update opensource_project_lms.py

--- opensource_project_lms.py
+++ opensource_project_lms.py
@@ -1,9 +1,19 @@
+from telnetlib import EC
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 import time
 
-driver = webdriver.Chrome()
+from selenium.webdriver.support.wait import WebDriverWait
+
+options = Options()
+options.add_argument("--start-maximized")
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options)
 
 driver.get('https://lms.hanbat.ac.kr/')
 
@@ -14,17 +24,21 @@ id_box.send_keys('20232071')
 
 pw_box = driver.find_element(By.ID, 'login_user_password')
 pw_box.click()
-time.sleep(1)
 pw_box.send_keys('cho182931!')
-
-time.sleep(1)
 login_btn = driver.find_element(By.CLASS_NAME, 'login_btn')
 login_btn.click()
 
-time.sleep(5)
+time.sleep(1)
 
 firstclass_btn = driver.find_element(By.CLASS_NAME, 'ic-DashboardCard')
 firstclass_btn.click()
 
-time.sleep(10)
+time.sleep(1)
 
+no1_btn = driver.find_element(By.CLASS_NAME, 'context_external_tool_18')
+no1_btn.click()
+time.sleep(8)
+
+
+
+time.sleep(15)
