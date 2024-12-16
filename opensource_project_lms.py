3d35e7c0ad96cc1b6dbb080b02dd0ef2fdd26e78
junyong23 <junyong5328@naver.com>
2024-12-17 02:35:13 +0900

    Update opensource_project_lms.py

--- opensource_project_lms.py
+++ opensource_project_lms.py
@@ -1,5 +1,5 @@
 from telnetlib import EC
-
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -29,16 +29,22 @@ login_btn = driver.find_element(By.CLASS_NAME, 'login_btn')
 login_btn.click()
 
 time.sleep(1)
-
-firstclass_btn = driver.find_element(By.CLASS_NAME, 'ic-DashboardCard')
-firstclass_btn.click()
+calen_btn = driver.find_element(By.ID, 'global_nav_calendar_link')
+calen_btn.click()
 
 time.sleep(1)
 
-no1_btn = driver.find_element(By.CLASS_NAME, 'context_external_tool_18')
-no1_btn.click()
-time.sleep(8)
+now = datetime.now()
+month = now.month
+file_name = f"C:/Users/junyo/OneDrive/바탕 화면/{month}월 일정.txt"
+file_header = f"{month}월의 일정 입니다.\n"
 
+f = open(file_name, "w")
+f.write(file_header)
+if(month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
+    for i in range(1, 32, 1):
+        f.write("Hello")
+f.close()
 
 
 time.sleep(15)
