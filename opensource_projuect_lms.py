f255d006b576dab91d5035aea12847a13cd0b151
junyong23 <junyong5328@naver.com>
2024-12-16 21:57:58 +0900

    lms hanbat login page 열기

--- opensource_projuect_lms.py
+++ opensource_projuect_lms.py
@@ -1,2 +1,3 @@
-print("hello")
-print("This is for test")
+import webbrowser
+
+webbrowser.open_new('https://eclass.hanbat.ac.kr/xn-sso/login.php?auto_login=&sso_only=&cvs_lgn=&site=&return_url=https%3A%2F%2Feclass.hanbat.ac.kr%2Fxn-sso%2Fgw-cb.php%3Ffrom%3D%26site%3D%26login_type%3Dstandalone%26return_url%3D')
